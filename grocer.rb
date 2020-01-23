18ebfed2202eff004be531e6f12d8bc1d5e06736
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-23 22:45:47 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -87,7 +87,19 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  
+   total = 0
+  i = 0
+
+  ccart = consolidate_cart(cart)
+  apply_coupons(ccart, coupons)
+  apply_clearance(ccart)
+
+  while i < ccart.length do
+    total += items_total_cost(ccart[i])
+    i += 1
+  end
+
+  total >= 100 ? total * (1.0 - BIG_PURCHASE_DISCOUNT_RATE) : total
   # Consult README for inputs and outputs
   #
   # This method should call
@@ -98,3 +110,5 @@ def checkout(cart, coupons)
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
 end
+
+
