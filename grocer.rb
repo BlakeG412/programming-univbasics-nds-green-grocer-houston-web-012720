21e2b1dcf62c8c11acd52c41f866dcfd3752188d
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-23 22:42:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,6 +33,8 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 end
 
+
+
 def apply_coupons(cart, coupons)
    i = 0
   while i < coupons.count do
