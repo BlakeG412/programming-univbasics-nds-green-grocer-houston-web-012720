989e6a2b8e9d37b0ad7170f4c38773dd921dbe8e
Blake Gaal <bag.blakeandrewgaal@gmail.com>
2020-01-23 22:33:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -11,7 +11,22 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
- 
+ i = 0
+  result = []
+
+  while i < cart.count do
+    item_name = cart[i][:item]
+    sought_item = find_item_by_name_in_collection(item_name, result)
+    if sought_item
+      sought_item[:count] += 1
+    else
+      cart[i][:count] = 1
+      result << cart[i]
+    end
+    i += 1
+  end
+
+  result
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
